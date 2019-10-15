56e2cbbbabbfc4fa814ff10feb42c9b312457a9c
Taylor <taylor@oakcity.io>
2019-10-15 12:02:14 -0400

    fix

--- index.test.js
+++ index.test.js
@@ -9,13 +9,3 @@ test('that weekNum returns a number', () => {
   expect(weekNum).toBeDefined();
 });
 
-const weekNum = require('./index.js');
-
-test('that weekNum returns a value', () => {
-  expect(weekNum).not.toBeNull();
-});
-
-// new test added
-test('that weekNum returns a number', () => {
-  expect(weekNum).toBeDefined();
-});
